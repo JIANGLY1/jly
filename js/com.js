5878a1877c9b4340c7991aca3f6fb56f2352b8d7
蒋小鱼 <apple@appledeMacBook-Pro.local>
2017-08-07 17:26:28 +0800

    二次修改个人主页

---
+++ js/com.js
@@ -0,0 +1,6 @@
+$(".jly-right-nav").bind("click",function(){	
+         		$(".jly-img-nav-cont").addClass("shw");
+         	});
+         	$(".close").bind("click",function(){
+         		$(".jly-img-nav-cont").removeClass("shw").addClass("hid");
+         	});
