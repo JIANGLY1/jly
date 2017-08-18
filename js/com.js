d943eee754ea610993d4bcd4a571dbd8616efd3a
蒋小鱼 <753278098@qq.com>
2017-08-18 14:52:37 +0800

    主页初步提交

---
+++ js/com.js
@@ -0,0 +1,6 @@
+$(".jly-right-nav").bind("click", function() {
+	$(".jly-img-nav-cont").addClass("shw");
+});
+$(".closes").bind("click", function() {
+	$(".jly-img-nav-cont").removeClass("shw").addClass("hid");
+});
