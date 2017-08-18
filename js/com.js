799615888a1dc135584f7fdc050fbe13d78ba48d
蒋小鱼 <753278098@qq.com>
2017-08-18 14:51:54 +0800

    删除

--- js/com.js
+++
@@ -1,6 +0,0 @@
-$(".jly-right-nav").bind("click",function(){	
-         		$(".jly-img-nav-cont").addClass("shw");
-         	});
-         	$(".close").bind("click",function(){
-         		$(".jly-img-nav-cont").removeClass("shw").addClass("hid");
-         	});
